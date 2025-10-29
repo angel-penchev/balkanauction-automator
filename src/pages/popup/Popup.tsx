6aceeafb2de17328f0eaac6cf9647265130ca198
Angel Penchev <angel@penchev.dev>
2025-10-30 01:38:27 +0200

    feat: implement auction tab query in popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React, { useState } from 'react';
+import Browser from 'webextension-polyfill';
+import type { Tabs } from 'webextension-polyfill';
 
 export default function Popup() {
+  const [message, setMessage] = useState("")
+
+  async function thang() {
+    let auctionTabs = await Browser.tabs.query({
+      url: "https://balkanauction.com/*/auction/*" // TODO: extract in a configurable value
+    });
+    console.log(auctionTabs);
+    const tabUrls = auctionTabs.map(tab => tab.url).join("\n");
+    setMessage(tabUrls)
+  };
+
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
       <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
+        <button onClick={_ => thang()}>
+          do thah thang1
+        </button>
+        {message}
       </header>
     </div>
   );
